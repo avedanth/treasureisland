1bed198ba66a14de75c6c054a1fbe911d42f5f35
avedanth <71074777+avedanth@users.noreply.github.com>
2021-06-02 15:57:58 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("F C U N T O I N", 100,250)
+    fill("lightblue")
+
+
+    fill("white")
+    textSize(15)
+    text("D S A B A E T A", 700,150)
+    fill("lightblue")
+}
